05d14ea65af5ead38d35e89dcea761241ae2281f
Kaiden Flaig <kaidenflaig335@gmail.com>
2024-09-07 22:58:57 -0600

    removed duplicate import

--- src/fileService.js
+++ src/fileService.js
@@ -1,7 +1,6 @@
 import fs from "fs";
 import { exitWithCode, readLine } from "./utils.js";
 import path from "path";
-import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
